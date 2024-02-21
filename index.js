bacfd9e5eff168618cffdd8ea2eb2a5298931d14
levent2607 <levent.o@web.de>
2024-02-21 12:05:19 +0100

    feat: changed index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+    res.send(`
+    <h1>Willkommen auf der Startseite</h1>
+    <p>Hier ist etwas zusätzlicher Text.</p>
+    <p>Du kannst auch andere HTML-Elemente verwenden.</p>
+  `);
 
 
 const server = app.listen(port, () => {
