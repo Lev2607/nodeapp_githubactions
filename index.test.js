93de222ac700b50cb7493419f3a0ff5420cbcb85
levent2607 <levent.o@web.de>
2024-02-21 12:09:21 +0100

    feat: changed index.js

--- index.test.js
+++ index.test.js
@@ -6,9 +6,11 @@ afterAll(done => {
 });
 
 describe('GET /', () => {
-  it('responds with Hello, World!', async () => {
+  it('responds with the expected HTML', async () => {
     const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toContain('<h1>Willkommen auf der Startseite</h1>');
+    expect(response.text).toContain('<p>Hier ist etwas zusätzlicher Text.</p>');
+    expect(response.text).toContain('<p>Du kannst auch andere HTML-Elemente verwenden.</p>');
   });
 });
